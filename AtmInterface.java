049ad5ce5140651fecdf083612947e28b121e190
Nayan Nagpal <110152854+nayannagpal@users.noreply.github.com>
2023-03-28 18:42:49 +0530

    Update AtmInterface.java

--- AtmInterface.java
+++ AtmInterface.java
@@ -10,12 +10,6 @@ class BankAccount {
 	int transactions = 0;
 	String transactionHistory = "";
 	
-	// BankAccount(String name, String userName, String password, String accountNo) {
-	// this.name = name;
-	// this.userName = userName;
-	// this.password = password;
-	// this.accountNo = accountNo;
-	
 	
 	public void register() {
 		try (Scanner sc = new Scanner(System.in)) {
